ff00df26e06961d620d24d748d622968785e216d
Suresh Alse <alseambusher@gmail.com>
2012-10-08 15:46:20 +0530

    first

---
+++ config.php
@@ -0,0 +1,8 @@
+<?
+$config= array();
+$config['base_url']='http://localhost/mockstock';
+$config['db_host']='localhost';
+$config['db_username']='root';
+$config['db_password']='alse';
+$config['db_database']='mockstock';
+?>
